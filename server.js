e5d76c5440ef735143748b57ef29cada13bd64e1
nusaybazman41-ops <nusaybazman41@gmail.com>
2026-06-14 17:54:59 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ptero Clone Panel Running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
