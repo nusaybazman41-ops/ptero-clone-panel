5a86b71e6fc6261e8bfac146d996ec0f671debdc
nusaybazman41-ops <nusaybazman41@gmail.com>
2026-06-14 18:04:55 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,25 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Ptero Clone Panel Running!");
+// login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if(username === "admin" && password === "1234"){
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, message: "Wrong login" });
+  }
+});
+
+// servers
+app.get("/api/servers", (req, res) => {
+  res.json([
+    { name: "Minecraft Server", status: "Running" },
+    { name: "CSGO Server", status: "Stopped" }
+  ]);
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Running...");
 });
